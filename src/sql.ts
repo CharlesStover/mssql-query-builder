ab2c88dd8ca0c87b07acfa8c9fcc809ef1735c38
Charles Stover <charles.stover@walmart.com>
2018-07-23 17:18:23 -0500

    object -> map

--- src/sql.ts
+++ src/sql.ts
@@ -12,56 +12,70 @@ type RejectPool = (error: ConnectionError | Error) => void;
 type ResolvePool = (pool: ConnectionPool) => void;
 
 // Cache of SQL Pools.
-interface SqlPools {
-  [server: string]: {
-    [user: string]: {
-      [database: string]: Promise<ConnectionPool>
-    }
-  };
-}
-const sqlPools: SqlPools = {};
+type SqlDatabases = Map<string, Promise<ConnectionPool>>;
+type SqlPools = Map<string, SqlUsers>;
+type SqlUsers = Map<string | symbol, SqlDatabases>;
+
+const sqlPools: SqlPools = new Map<string, SqlUsers>();
 
 // Return a pool from the cache if one exists.
 // Otherwise, create and cache that pool.
 export const sqlPool = async (c: config = defaultConfig): Promise<ConnectionPool> => {
 
+  // Create a cache for this server.
+  if (!sqlPools.has(c.server)) {
+    sqlPools.set(c.server, new Map<string | symbol, SqlDatabases>());
+  }
+
+  const users = sqlPools.get(c.server);
+  if (!users) {
+    throw new Error('Could not allocate SQL user.');
+  }
+
   // Create an object key for connections that do not have usernames.
   const poolUsername: string | symbol = c.user || Symbol.for('undefined');
 
-  // Create a cache for this server.
-  if (!Object.prototype.hasOwnProperty.call(sqlPools, c.server)) {
-    sqlPools[c.server] = Object.create(null);
+  // Create a cache for this user on this server.
+  if (!users.has(poolUsername)) {
+    users.set(poolUsername, new Map<string, Promise<ConnectionPool>>());
   }
 
-  // Create a cache for this user on this server.
-  if (!Object.prototype.hasOwnProperty.call(sqlPools[c.server], poolUsername)) {
-    sqlPools[c.server][poolUsername] = Object.create(null);
+  const connections = users.get(poolUsername);
+  if (!connections) {
+    throw new Error('Could not allocation SQL connection.');
   }
 
   // Create the pool for this database for this user on this server.
-  if (!Object.prototype.hasOwnProperty.call(sqlPools[c.server][poolUsername], c.database)) {
-    sqlPools[c.server][poolUsername][c.database] = new Promise(
-      (resolve: ResolvePool, reject: RejectPool) => {
-        const pool: ConnectionPool = new ConnectionPool(
-          c,
-          (error: ConnectionError | Error | null) => {
-            if (error) {
-              reject(error);
+  if (!connections.has(c.database)) {
+    connections.set(
+      c.database,
+      new Promise(
+        (resolve: ResolvePool, reject: RejectPool) => {
+          const pool: ConnectionPool = new ConnectionPool(
+            c,
+            (error: ConnectionError | Error | null) => {
+              if (error) {
+                reject(error);
+              }
+              else {
+                resolve(pool);
+              }
             }
-            else {
-              resolve(pool);
-            }
-          }
-        );
-      }
-    )
-    .catch((error: ConnectionError | Error) => {
-      throw error;
-    });
+          );
+        }
+      )
+      .catch((error: ConnectionError | Error) => {
+        throw error;
+      })
+    );
   }
 
   // Return the pool for this database for this user on this server.
-  return sqlPools[c.server][poolUsername][c.database];
+  const pool = connections.get(c.database);
+  if (!pool) {
+    throw new Error('Could not allocate SQL pool.');
+  }
+  return pool;
 };
 
 export const sqlRequest = async (c: config = defaultConfig): Promise<Request> => {
